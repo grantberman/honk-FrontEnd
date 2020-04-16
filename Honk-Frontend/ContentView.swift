005a92d116cb6e05596c2b1b8548b5f8e66587b9
Nick O'Leary <noleary82@gmail.com>
2020-04-16 15:19:37 -0400

    working on scrolling

--- Honk-Frontend/ContentView.swift
+++ Honk-Frontend/ContentView.swift
@@ -95,28 +95,17 @@ struct ContentView: View {
         VStack {
             NavigationView {
                 CustomScrollView(reversed: true) {
-                    ForEach(self.objects, id: \.self) { object in
-                        VStack {
-                            Text("Row \(object)").padding().background(Color.yellow)
-                            NavigationLink(destination: Text("Details for \(object)")) {
-                            Image(systemName: "chevron.right.circle")
+
+                        ForEach (self.chatController.messages, id: \.self) { msg in
+                            VStack{
+                                ChatRow(chatMessage: msg)
                             }
-                            Divider()
-                            }.overlay(RoundedRectangle(cornerRadius: 8).stroke())
-                        }
+                        
+                        }.onAppear {
+                            UITableView.appearance().separatorStyle = .none     //gets rid of lines between messages
+                            }
                     }
-                    .navigationBarTitle("Reverse", displayMode: .inline)
-                List {
-                    ForEach (chatController.messages, id: \.self) { msg in
-                        ChatRow(chatMessage: msg)
-                        .rotationEffect(.radians(.pi))
-                        .scaleEffect(x: -1, y: 1, anchor: .center)
-                    }
-                }.onAppear {
-                    UITableView.appearance().separatorStyle = .none     //gets rid of lines between messages
-                }
-                .rotationEffect(.radians(.pi))
-                .scaleEffect(x: -1, y: 1, anchor: .center)
+                    .navigationBarTitle("Chat Title", displayMode: .inline)
             }
             HStack{
                 TextField("Message...", text: $composedMessage).frame(minHeight: CGFloat(30)).background(GeometryGetter(rect: $kGuardian.rects[0]))
@@ -130,98 +119,7 @@ struct ContentView: View {
         .onDisappear { self.kGuardian.removeObserver() }
     }
     
-    struct CustomScrollView<Content>: View where Content: View {
-        var axes: Axis.Set = .vertical
-        var reversed: Bool = false
-        var scrollToEnd: Bool = false
-        var content: () -> Content
 
-        @State private var contentHeight: CGFloat = .zero
-        @State private var contentOffset: CGFloat = .zero
-        @State private var scrollOffset: CGFloat = .zero
-
-        var body: some View {
-            GeometryReader { geometry in
-                if self.axes == .vertical {
-                    self.vertical(geometry: geometry)
-                } else {
-                    // implement same for horizontal orientation
-                }
-            }
-            .clipped()
-        }
-
-        private func vertical(geometry: GeometryProxy) -> some View {
-            VStack {
-                content()
-            }
-            .modifier(ViewHeightKey())
-            .onPreferenceChange(ViewHeightKey.self) {
-                self.updateHeight(with: $0, outerHeight: geometry.size.height)
-            }
-            .frame(height: geometry.size.height, alignment: (reversed ? .bottom : .top))
-            .offset(y: contentOffset + scrollOffset)
-            .animation(.easeInOut)
-            .background(Color.white)
-            .gesture(DragGesture()
-                .onChanged { self.onDragChanged($0) }
-                .onEnded { self.onDragEnded($0, outerHeight: geometry.size.height) }
-            )
-        }
-
-        private func onDragChanged(_ value: DragGesture.Value) {
-            self.scrollOffset = value.location.y - value.startLocation.y
-        }
-
-        private func onDragEnded(_ value: DragGesture.Value, outerHeight: CGFloat) {
-            let scrollOffset = value.predictedEndLocation.y - value.startLocation.y
-
-            self.updateOffset(with: scrollOffset, outerHeight: outerHeight)
-            self.scrollOffset = 0
-        }
-
-        private func updateHeight(with height: CGFloat, outerHeight: CGFloat) {
-            let delta = self.contentHeight - height
-            self.contentHeight = height
-            if scrollToEnd {
-                self.contentOffset = self.reversed ? height - outerHeight - delta : outerHeight - height
-            }
-            if abs(self.contentOffset) > .zero {
-                self.updateOffset(with: delta, outerHeight: outerHeight)
-            }
-        }
-
-        private func updateOffset(with delta: CGFloat, outerHeight: CGFloat) {
-            let topLimit = self.contentHeight - outerHeight
-
-            if topLimit < .zero {
-                 self.contentOffset = .zero
-            } else {
-                var proposedOffset = self.contentOffset + delta
-                if (self.reversed ? proposedOffset : -proposedOffset) < .zero {
-                    proposedOffset = 0
-                } else if (self.reversed ? proposedOffset : -proposedOffset) > topLimit {
-                    proposedOffset = (self.reversed ? topLimit : -topLimit)
-                }
-                self.contentOffset = proposedOffset
-            }
-        }
-    }
-
-    struct ViewHeightKey: PreferenceKey {
-        static var defaultValue: CGFloat { 0 }
-        static func reduce(value: inout Value, nextValue: () -> Value) {
-            value = value + nextValue()
-        }
-    }
-
-    extension ViewHeightKey: ViewModifier {
-        func body(content: Content) -> some View {
-            return content.background(GeometryReader { proxy in
-                Color.clear.preference(key: Self.self, value: proxy.size.height)
-            })
-        }
-    }
     
     
     func sendMessage() {
@@ -231,6 +129,101 @@ struct ContentView: View {
     
 }
 
+
+// To get the automatic scroll
+struct CustomScrollView<Content>: View where Content: View {
+    var axes: Axis.Set = .vertical
+    var reversed: Bool = false
+    var content: () -> Content
+
+    @State private var contentHeight: CGFloat = .zero
+    @State private var contentOffset: CGFloat = .zero
+    @State private var scrollOffset: CGFloat = .zero
+
+    var body: some View {
+        GeometryReader { geometry in
+            if self.axes == .vertical {
+                self.vertical(geometry: geometry)
+            } else {
+                // implement same for horizontal orientation
+            }
+        }
+        .clipped()
+    }
+
+    private func vertical(geometry: GeometryProxy) -> some View {
+        VStack {
+            content()
+        }
+        .modifier(ViewHeightKey())
+        .onPreferenceChange(ViewHeightKey.self) {
+            self.updateHeight(with: $0, outerHeight: geometry.size.height)
+        }
+        .frame(height: geometry.size.height, alignment: (reversed ? .bottom : .top))
+        .offset(y: contentOffset + scrollOffset)
+        .animation(.easeInOut)
+        .background(Color.white)
+        .gesture(DragGesture()
+            .onChanged { self.onDragChanged($0) }
+            .onEnded { self.onDragEnded($0, outerHeight: geometry.size.height) }
+        )
+    }
+
+    private func onDragChanged(_ value: DragGesture.Value) {
+        self.scrollOffset = value.location.y - value.startLocation.y
+    }
+
+    private func onDragEnded(_ value: DragGesture.Value, outerHeight: CGFloat) {
+        let scrollOffset = value.predictedEndLocation.y - value.startLocation.y
+
+        self.updateOffset(with: scrollOffset, outerHeight: outerHeight)
+        self.scrollOffset = 0
+    }
+
+    private func updateHeight(with height: CGFloat, outerHeight: CGFloat) {
+        let delta = self.contentHeight - height
+        self.contentHeight = height
+        if abs(self.contentOffset) > .zero {
+            self.updateOffset(with: delta, outerHeight: outerHeight)
+        }
+    }
+
+    // Note I think this is where the large space is coming from
+    private func updateOffset(with delta: CGFloat, outerHeight: CGFloat) {
+        let topLimit = self.contentHeight - outerHeight
+
+        if topLimit < .zero {
+             self.contentOffset = .zero
+        } else {
+            var proposedOffset = self.contentOffset + delta
+            if (self.reversed ? proposedOffset : -proposedOffset) < .zero {
+                proposedOffset = 0
+            } else if (self.reversed ? proposedOffset : -proposedOffset) > topLimit {
+                proposedOffset = (self.reversed ? topLimit : -topLimit)
+            }
+            self.contentOffset = proposedOffset
+        }
+    }
+}
+
+struct ViewHeightKey: PreferenceKey {
+    static var defaultValue: CGFloat { 0 }
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        value = value + nextValue()
+    }
+}
+
+extension ViewHeightKey: ViewModifier {
+    func body(content: Content) -> some View {
+        return content.background(GeometryReader { proxy in
+            Color.clear.preference(key: Self.self, value: proxy.size.height)
+        })
+    }
+}
+
+
+
+// original stuff
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
@@ -238,6 +231,9 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
+
+
+// fixing the keyboard part
 struct GeometryGetter: View {
     @Binding var rect: CGRect
 
