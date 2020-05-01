29e3a7ab10b43cb29d2de7b3970f64f89f29a01a
grant berman <grantberman1live.com>
2020-05-01 00:19:33 -0400

    this update includes a new part of the chat view that allows the user to switch between chats inside of a community with a new SideMenu view. This is in preparation for storing local community, chat, and message data

---
+++ Honk-Frontend/Models/Chat.swift
@@ -0,0 +1,9 @@
+//
+//  Chat.swift
+//  Honk-Frontend
+//
+//  Created by Grant Berman on 4/30/20.
+//  Copyright © 2020 Grant Berman. All rights reserved.
+//
+
+import Foundation
---
+++ Honk-Frontend/SideMenu.swift
@@ -0,0 +1,25 @@
+//
+//  MenuView.swift
+//  Honk-Frontend
+//
+//  Created by Grant Berman on 4/30/20.
+//  Copyright © 2020 Grant Berman. All rights reserved.
+//
+//
+import SwiftUI
+
+struct MenuView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        Text("hello world")
+
+        }
+    }
+
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
